59cffae6851be54592a86024ab4ddc299dff50c5
Cristian Dolinta <cristian.sw@protonmail.com>
2021-02-11 15:08:35 +0200

    Added basic POJO model. Close #2

---
+++ src/main/java/clinic/dentalclinic/model/Doctor.java
@@ -0,0 +1,11 @@
+package clinic.dentalclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Doctor extends Person {
+    private Integer cabinetNumber;
+
+}
---
+++ src/main/java/clinic/dentalclinic/model/Patient.java
@@ -0,0 +1,14 @@
+package clinic.dentalclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Patient extends Person {
+    private String phoneNumber;
+    private LocalDate birthDate;
+
+}
