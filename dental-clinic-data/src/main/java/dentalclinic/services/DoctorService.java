ee9ba7f36a6cafdc5ff0c9264a8b883078920b7e
Cristian Dolinta <cristian.sw@protonmail.com>
2021-04-20 10:31:04 +0300

    Refactored to base service interface. Close #11

--- dental-clinic-data/src/main/java/dentalclinic/services/DoctorService.java
+++ dental-clinic-data/src/main/java/dentalclinic/services/DoctorService.java
@@ -3,14 +3,7 @@ package dentalclinic.services;
 import dentalclinic.model.Doctor;
 
 
-import java.util.Set;
 
-public interface DoctorService {
+public interface DoctorService extends CrudService<Doctor, Long> {
     Doctor findByLastName(String lastName);
-
-    Doctor findById(Long id);
-
-    Doctor save(Doctor doctor);
-
-    Set<Doctor> findAll();
 }
--- dental-clinic-data/src/main/java/dentalclinic/services/PatientService.java
+++ dental-clinic-data/src/main/java/dentalclinic/services/PatientService.java
@@ -2,14 +2,7 @@ package dentalclinic.services;
 
 import dentalclinic.model.Patient;
 
-import java.util.Set;
 
-public interface PatientService {
+public interface PatientService extends CrudService<Patient, Long> {
     Patient findByLastName(String lastName);
-
-    Patient findById(Long id);
-
-    Patient save(Patient patient);
-
-    Set<Patient> findAll();
 }
