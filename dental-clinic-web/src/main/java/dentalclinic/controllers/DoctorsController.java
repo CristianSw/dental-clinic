7320739707cd1d2fd4128fe3f860690f88e708d8
Cristian Dolinta <cristian.sw@protonmail.com>
2021-04-21 18:40:37 +0300

    Added index page for doctors and controller for it. Close #13

---
+++ dental-clinic-web/src/main/java/dentalclinic/controllers/DoctorsController.java
@@ -0,0 +1,13 @@
+package dentalclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class DoctorsController {
+    @RequestMapping({"/doctors/index.html","/docs","/doctors"})
+    public String index(){
+        return "/doctors/index";
+    }
+}
