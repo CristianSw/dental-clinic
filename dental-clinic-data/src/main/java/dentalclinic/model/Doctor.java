de17f57d050e957f538ad03f4c132feda99150e6
Cristian Dolinta <cristian.sw@protonmail.com>
2021-04-25 19:34:03 +0300

    Converted doctors to JPA. Close #31

--- dental-clinic-data/src/main/java/dentalclinic/model/Doctor.java
+++ dental-clinic-data/src/main/java/dentalclinic/model/Doctor.java
@@ -5,11 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "doctors")
 public class Doctor extends Person {
+    @Column(name = "cabinet_number")
     private Integer cabinetNumber;
 
 }
