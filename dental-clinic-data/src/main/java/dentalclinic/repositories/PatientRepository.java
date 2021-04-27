f6bc4039e3f47aaebe9690a45237775b003d20b4
Cristian Dolinta <cristian.sw@protonmail.com>
2021-04-27 17:55:00 +0300

    Created SDJPA service for patients. Close #33

--- dental-clinic-data/src/main/java/dentalclinic/repositories/PatientRepository.java
+++ dental-clinic-data/src/main/java/dentalclinic/repositories/PatientRepository.java
@@ -4,4 +4,6 @@ import dentalclinic.model.Patient;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PatientRepository extends CrudRepository<Patient, Long> {
+
+    Patient findByLastName(String lastName);
 }
---
+++ dental-clinic-data/src/main/java/dentalclinic/services/springdatajpa/PatientSDJPAService.java
@@ -0,0 +1,55 @@
+package dentalclinic.services.springdatajpa;
+
+import dentalclinic.model.Patient;
+import dentalclinic.repositories.PatientRepository;
+import dentalclinic.services.PatientService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+@Service
+@Profile("springdatajpa")
+public class PatientSDJPAService implements PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientSDJPAService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @Override
+    public Set<Patient> findAll() {
+
+        Set<Patient> patients = new HashSet<>();
+
+        patientRepository.findAll().forEach(patients::add);
+        return patients;
+    }
+
+    @Override
+    public Patient findById(Long aLong) {
+        return patientRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Patient save(Patient object) {
+        return patientRepository.save(object);
+    }
+
+    @Override
+    public void delete(Patient object) {
+    patientRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+    patientRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Patient findByLastName(String lastName) {
+        return patientRepository.findByLastName(lastName);
+    }
+}
