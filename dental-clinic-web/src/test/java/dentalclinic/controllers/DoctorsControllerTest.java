86c8256efda1e0fae61a6fcba019e616dfb58756
Cristian Dolinta <cristian.sw@protonmail.com>
2021-04-28 19:53:08 +0300

    Confiured for using Juint 5 tests. Close #4

---
+++ dental-clinic-web/src/test/java/dentalclinic/controllers/DoctorsControllerTest.java
@@ -0,0 +1,16 @@
+package dentalclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DoctorsControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void index() {
+    }
+}
