93bce19f67a3f8b1a7f9f185c8a9d2e15d8c8bf0
Cristian Dolinta <cristian.sw@protonmail.com>
2021-04-29 13:59:17 +0300

    Writed CRUD tests for Doctor Map implemetation, added findByLastName implementation. Close #51

---
+++ dental-clinic-data/src/test/java/dentalclinic/services/map/DoctorServiceMapTest.java
@@ -0,0 +1,36 @@
+package dentalclinic.services.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DoctorServiceMapTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
